105414e54574f9e31fe83376f36ce87a5ddf9867
DeepaKabilan <deepaabsm@gmail.com>
2023-02-02 08:58:08 +0530

    Basic

---
+++ src/org/practice/Practice.java
@@ -0,0 +1,11 @@
+package org.practice;
+
+public class Practice {
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String s2 = "Selenium";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}
